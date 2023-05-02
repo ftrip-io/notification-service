e17f5f7559a807d06a32649672d966d7f94accdc
Panic <panic.milos99@gmail.com>
2023-05-02 15:08:50 +0200

    feat: test project, framework setup, dockerfiles

--- ftrip.io.notification-service/ftrip.io.notification-service.contracts/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ftrip.io.notification_service.contracts
-{
-    public class Class1
-    {
-    }
-}
--- ftrip.io.notification-service/ftrip.io.notification-service/Startup.cs
+++ ftrip.io.notification-service/ftrip.io.notification-service/Startup.cs
@@ -1,15 +1,21 @@
+using ftrip.io.framework.auth;
+using ftrip.io.framework.CQRS;
+using ftrip.io.framework.ExceptionHandling.Extensions;
+using ftrip.io.framework.Globalization;
+using ftrip.io.framework.HealthCheck;
+using ftrip.io.framework.Installers;
+using ftrip.io.framework.Mapping;
+using ftrip.io.framework.messaging.Installers;
+using ftrip.io.framework.Persistence.NoSql.Mongodb.Installers;
+using ftrip.io.framework.Secrets;
+using ftrip.io.framework.Swagger;
+using ftrip.io.framework.Validation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ftrip.io.notification_service
 {
@@ -26,6 +32,19 @@ namespace ftrip.io.notification_service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            InstallerCollection.With(
+                new SwaggerInstaller<Startup>(services),
+                new HealthCheckUIInstaller(services),
+                new AutoMapperInstaller<Startup>(services),
+                new FluentValidationInstaller<Startup>(services),
+                new GlobalizationInstaller<Startup>(services),
+                new EnviromentSecretsManagerInstaller(services),
+                new JwtAuthenticationInstaller(services),
+                new MongodbInstaller(services),
+                new MongodbHealthCheckInstaller(services),
+                new CQRSInstaller<Startup>(services),
+                new RabbitMQInstaller<Startup>(services, RabbitMQInstallerType.Publisher | RabbitMQInstallerType.Consumer)
+            ).Install();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,12 +59,24 @@ namespace ftrip.io.notification_service
 
             app.UseRouting();
 
+            app.UseCors(policy => policy
+               .WithOrigins(Environment.GetEnvironmentVariable("API_PROXY_URL"))
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+            );
+
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseFtripioGlobalExceptionHandler();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            app.UseFtripioSwagger(Configuration.GetSection(nameof(SwaggerUISettings)).Get<SwaggerUISettings>());
+            app.UseFtripioHealthCheckUI(Configuration.GetSection(nameof(HealthCheckUISettings)).Get<HealthCheckUISettings>());
         }
     }
 }
