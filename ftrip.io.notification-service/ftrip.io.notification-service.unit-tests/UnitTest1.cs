e6a9c8e1ce7f27d5ed9da9f67e3b81e77a887d8b
Panic <panic.milos99@gmail.com>
2023-05-02 17:08:12 +0200

    Setup/initial project (#3)
    * feat: initial notification service with contracts library
    
    * feat: test project, framework setup, dockerfiles

---
+++ ftrip.io.notification-service/ftrip.io.notification-service.unit-tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using Xunit;
+
+namespace ftrip.io.notification_service.unit_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
