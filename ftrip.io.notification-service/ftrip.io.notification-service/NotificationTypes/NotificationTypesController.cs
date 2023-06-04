c3505fe62d1a40005cd9effdb289e21d2796a8f2
Panic <panic.milos99@gmail.com>
2023-06-04 21:54:23 +0200

    feat: authorized endpoints

--- ftrip.io.notification-service/ftrip.io.notification-service/NotificationTypes/NotificationTypesController.cs
+++ ftrip.io.notification-service/ftrip.io.notification-service/NotificationTypes/NotificationTypesController.cs
@@ -1,5 +1,6 @@
 ﻿using ftrip.io.notification_service.NotificationTypes.UseCases.ReadByUserType;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace ftrip.io.notification_service.NotificationTypes
             _mediator = mediator;
         }
 
+        [Authorize]
         [HttpGet]
         public async Task<IActionResult> Read([FromQuery] string userType, CancellationToken cancellationToken = default)
         {
