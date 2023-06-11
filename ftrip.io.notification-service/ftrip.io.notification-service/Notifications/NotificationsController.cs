461449edf73ff121fbf529b6ee502aa060527c9e
Panic <panic.milos99@gmail.com>
2023-06-11 22:07:54 +0200

    feat: authorized endpoints (#10)

--- ftrip.io.notification-service/ftrip.io.notification-service/Notifications/NotificationsController.cs
+++ ftrip.io.notification-service/ftrip.io.notification-service/Notifications/NotificationsController.cs
@@ -1,10 +1,13 @@
-﻿using ftrip.io.notification_service.Notifications.UseCases.CountByUserId;
+﻿using ftrip.io.notification_service.Attributes;
+using ftrip.io.notification_service.Notifications.UseCases.CountByUserId;
 using ftrip.io.notification_service.Notifications.UseCases.Delete;
 using ftrip.io.notification_service.Notifications.UseCases.MarkAsSeen;
 using ftrip.io.notification_service.Notifications.UseCases.MarkAsUnseen;
 using ftrip.io.notification_service.Notifications.UseCases.ReadByUserId;
 using ftrip.io.notification_service.Notifications.UseCases.Save;
+using ftrip.io.user_service.Attributes;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,12 +25,16 @@ namespace ftrip.io.notification_service.Notifications
             _mediator = mediator;
         }
 
+        [Authorize]
+        [UserSpecific]
         [HttpGet("{userId}/notifications")]
         public async Task<IActionResult> Read(string userId, [FromQuery] bool? seen, CancellationToken cancellationToken = default)
         {
             return Ok(await _mediator.Send(new ReadByUserIdQuery() { UserId = userId, Seen = seen }, cancellationToken));
         }
 
+        [Authorize]
+        [UserSpecific]
         [HttpGet("{userId}/notifications/count")]
         public async Task<IActionResult> Count(string userId, [FromQuery] bool? seen, CancellationToken cancellationToken = default)
         {
@@ -41,18 +48,27 @@ namespace ftrip.io.notification_service.Notifications
             return Ok(await _mediator.Send(request, cancellationToken));
         }
 
+        [Authorize]
+        [UserSpecific]
+        [UserSpecificNotification]
         [HttpPut("{userId}/notifications/{notificationId}/seen")]
         public async Task<IActionResult> Seen(string notificationId, CancellationToken cancellationToken = default)
         {
             return Ok(await _mediator.Send(new MarkNotificationAsSeenRequest() { NotificationId = notificationId }, cancellationToken));
         }
 
+        [Authorize]
+        [UserSpecific]
+        [UserSpecificNotification]
         [HttpPut("{userId}/notifications/{notificationId}/unseen")]
         public async Task<IActionResult> Unseen(string notificationId, CancellationToken cancellationToken = default)
         {
             return Ok(await _mediator.Send(new MarkNotificationAsUnseenRequest() { NotificationId = notificationId }, cancellationToken));
         }
 
+        [Authorize]
+        [UserSpecific]
+        [UserSpecificNotification]
         [HttpDelete("{userId}/notifications/{notificationId}")]
         public async Task<IActionResult> Delete(string notificationId, CancellationToken cancellationToken = default)
         {
